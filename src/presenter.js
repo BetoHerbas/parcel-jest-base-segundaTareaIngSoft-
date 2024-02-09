d5a46b8a9e4530e72cf1f05b53dae1b56001a35e
BetoHerbas <luis.herbas@ucb.edu.bo>
2024-02-08 22:57:22 -0400

    Añadiendo multiplicacion

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,11 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+const mulbtn = document.querySelector('#mult-button');
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -11,5 +13,14 @@ form.addEventListener("submit", (event) => {
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>" + firstNumber + " + " + secondNumber + " = " + sumar(firstNumber, secondNumber) + "</p>";
 });
+
+mulbtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log('click');
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+
+  div.innerHTML = "<p>" + firstNumber + " * " + secondNumber + " = " + multiplicar(firstNumber, secondNumber) + "</p>";
+}); 
